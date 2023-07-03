e5c6f5544b4cad45e561d8f74089178cac29eefe
alinakyrianova <alina.kyrianova@gmail.com>
2023-07-03 19:48:47 +0200

    lodash

--- src /js/02-video.js
+++ src /js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const PLAYER_CURRENT_TIME = 'videoplayer-current-time';
+const playerEl = document.querySelector('#vimeo-player');
+const player = new Player(playerEl);
 
+const saveCurrentTime = throttle(() => {
+  player.getCurrentTime().then(currentTime => {
+    localStorage.setItem(PLAYER_CURRENT_TIME, JSON.stringify(currentTime));
+  });
+}, 1000);
+player.on('timeupdate', saveCurrentTime);
+
+function restorePlaybackTime() {
+  const currentTime = localStorage.getItem(PLAYER_CURRENT_TIME);
+  player.setCurrentTime(currentTime || 0);
+}
+restorePlaybackTime();
